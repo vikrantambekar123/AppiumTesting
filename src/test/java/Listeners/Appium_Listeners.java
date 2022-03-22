fdafc7f4b5fa16bebddd3db4d402156016efa6a4
vikrant123 <vikrant123@gmail.com>
2022-03-22 15:40:48 +0530

    Listeners Changes

--- src/test/java/Listeners/Appium_Listeners.java
+++ src/test/java/Listeners/Appium_Listeners.java
@@ -1,37 +1,61 @@
 package Listeners;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Appium_Listeners implements ITestListener
+import PracticeBase.PracBase_App_Demos;
+
+public class Appium_Listeners extends PracBase_App_Demos implements ITestListener
+
 {
 
+	
 	@Override
 	public void onTestStart(ITestResult result) 
 	{
-		System.out.println("On Test Start"+result.toString());
+		System.out.println("On Test Start  :- "+result.getName());
+		File file  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(file, new File(result.getName()+".jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
 	@Override
 	public void onTestSuccess(ITestResult result) 
 	{
-
+		System.out.println("On Test Success  :- "+result.getName());
+//		File file  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		try {
+//			FileUtils.copyFile(file, new File(result.getName()+".jpg"));
+//		} catch (IOException e) 
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
-
 	
 	@Override
 	public void onTestFailure(ITestResult result) 
 	{
-		
+		System.out.println("On Test Fail  :- "+result.getName());
 	}
 
 	
 	@Override
 	public void onTestSkipped(ITestResult result)
 	{
-		// TODO Auto-generated method stub
+		System.out.println("On Test Skipped  :- "+result.getName());
 
 	}
 
@@ -39,6 +63,7 @@ public class Appium_Listeners implements ITestListener
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) 
 	{
+		System.out.println("On onTestFailedButWithinSuccessPercentage :- "+result.getName());
 
 	}
 
@@ -46,6 +71,7 @@ public class Appium_Listeners implements ITestListener
 	@Override
 	public void onTestFailedWithTimeout(ITestResult result)
 	{
+		System.out.println("On onTestFailedWithTimeout  :- "+result.getName());
 
 	}
 
@@ -53,14 +79,16 @@ public class Appium_Listeners implements ITestListener
 	@Override
 	public void onStart(ITestContext context)
 	{
-	
+		System.out.println("On onStart  :- "+context.getName());
+
 	}
 
 	
 	@Override
 	public void onFinish(ITestContext context) 
 	{
-	
+		System.out.println("On onFinish   "+context.getName());
+
 	}
 
 	
